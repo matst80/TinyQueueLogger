5fba888491ce1ee612ade6bb05c8b3cf23c0dfbe
Mats Tornberg <mats.tornberg@tretton37.com>
2019-08-31 10:08:32 +0200

    function part added

---
+++ FunctionsQueueLogger/FunctionQueueLoggerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using QueueLogger;
+using System;
+
+namespace FunctionsQueueLogger
+{
+    public static class FunctionQueueLoggerExtensions
+    {
+        public static IServiceCollection AddQueueLogger(this IServiceCollection service)
+        {
+            var logger = new QueueLogger(new Microsoft.Azure.ServiceBus.QueueClient(
+                    Environment.GetEnvironmentVariable("QueueLoggerConnectionString"),
+                    Environment.GetEnvironmentVariable("QueueLoggerQueue")
+                ));
+            service.AddSingleton(logger);
+
+            return service;
+        }
+
+        public static void Track(this ILogger logger, LogLevel level, string trackedId, object message, [System.Runtime.CompilerServices.CallerMemberName] string source = "")
+        {
+            QueueLogger.Instance?.Log(new TrackedMessage(trackedId, message)
+            {
+                TrackedId = trackedId,
+                Level = level,
+                Source = source,
+                Type = "Serverless"
+            });
+        }
+    }
+}
---
+++ FunctionsQueueLogger/QueueLogger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Azure.ServiceBus;
+using QueueLogger;
+
+namespace FunctionsQueueLogger
+{
+    public class QueueLogger
+    {
+        public QueueLogger(QueueClient client)
+        {
+            this.client = client;
+            Instance = this;
+        }
+
+        public QueueLogger(string connectionString, string queueName, string source) : this(new QueueClient(connectionString, queueName))
+        {
+
+        }
+
+        public QueueLogger(QueueLoggerOptions config) : this(new QueueClient(config.ConnectionString, config.Queue))
+        {
+        }
+
+
+        private readonly QueueClient client;
+        internal static QueueLogger Instance;
+
+        public void Log(TrackedMessage trackedMessage)
+        {
+            client?.SendAsync(trackedMessage.GetMessage());
+        }
+
+    }
+}
